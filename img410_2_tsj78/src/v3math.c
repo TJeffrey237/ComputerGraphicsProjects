427fbedfe962d24425db5b8cfc8be423f4aad9dd
Tyler Jeffrey <tyler_tj@icloud.com>
2026-02-10 13:53:54 -0700

    Added clamping to the angle function to prevent rounding errors.

--- img410_2_tsj78/src/v3math.c
+++ img410_2_tsj78/src/v3math.c
@@ -49,10 +49,18 @@ float v3_angle(float *a, float *b) {
     float magA = v3_length(a);
     float magB = v3_length(b);
     if(magA == 0.0f || magB == 0.0f) {
-        fprintf(stderr, "Error: One of the vectors used has a magnitude of 0.");
+        fprintf(stderr, "Error: One of the vectors used has a magnitude of 0.\n");
         return 0.0f;
     }
-    return acos(dot / (magA * magB));
+    // clamping
+    float cos_theta = dot / (magA * magB);
+    if(cos_theta > 1.0f) {
+        cos_theta = 1.0f;
+    }
+    if(cos_theta < -1.0f) {
+        cos_theta = -1.0f;
+    }
+    return acosf(cos_theta);
 }
 
 float v3_angle_quick(float *a, float *b) {
@@ -60,7 +68,7 @@ float v3_angle_quick(float *a, float *b) {
     float magA = v3_length(a);
     float magB = v3_length(b);
     if(magA == 0.0f || magB == 0.0f) {
-        fprintf(stderr, "Error: One of the vectors used has a magnitude of 0.");
+        fprintf(stderr, "Error: One of the vectors used has a magnitude of 0.\n");
         return 0.0f;
     }
     return dot / (magA * magB);
@@ -81,13 +89,13 @@ float v3_length(float *a) {
     float x = a[0] * a[0];
     float y = a[1] * a[1];
     float z = a[2] * a[2];
-    return sqrt(x + y + z);
+    return sqrtf(x + y + z);
 }
 
 void v3_normalize(float *dst, float *a) {
     float mag = v3_length(a);
     if(mag == 0.0f) {
-        fprintf(stderr, "Error: The vector used for normalization has a magnitude of 0.");
+        fprintf(stderr, "Error: The vector used for normalization has a magnitude of 0.\n");
         return;
     }
     dst[0] = a[0] / mag;
