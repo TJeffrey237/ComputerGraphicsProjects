ec44232dd8f7b9728ace4a732dd71c3c0e74e7bc
Tyler Jeffrey <tyler_tj@icloud.com>
2026-02-19 12:51:07 -0700

    Created intial project structure for Raycast.

---
+++ img410_3_tsj78/src/raycast.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "ppm.h"
+#include "v3math.h"
+
+int main(int argc, int **argv) {
+    // STEP 1: READ SCENE FILE
+    // STEP 2: ITERATE THROUGH OBJECTS FOR INTERSECTIONS
+    // STEP 3: WRITE TO PPM FILE
+}
