05eefe6d69bb647229600798e7d457a28c0b59a0
Tyler Jeffrey <tyler_tj@icloud.com>
2026-03-14 14:11:01 -0700

    Created basic help function for recursion.

--- img410_5_tsj78/include/renderer.h
+++ img410_5_tsj78/include/renderer.h
@@ -9,5 +9,8 @@
 
 void render(uint32_t width, uint32_t height, const Camera& cam,
             const std::vector<Shape*>& shapes, const std::vector<Light*>& lights, std::vector<uint8_t>& buffer);
+            
+void ray_trace(float* Ro, float* Rd,  const std::vector<Shape*>& shapes,
+            const std::vector<Light*>& lights, int depth, float* out_color);
 
 #endif
--- img410_5_tsj78/src/renderer.cpp
+++ img410_5_tsj78/src/renderer.cpp
@@ -1,4 +1,11 @@
 #include "renderer.h"
+#define MAX_DEPTH = 5
+
+void ray_trace(float* Ro, float* Rd,  const std::vector<Shape*>& shapes, const std::vector<Light*>& lights, int depth, float* out_color) {
+    if(depth > MAX_DEPTH) {
+        return;
+    }
+}
 
 void render(uint32_t width, uint32_t height, const Camera& cam,
             const std::vector<Shape*>& shapes, const std::vector<Light*>& lights, std::vector<uint8_t>& buffer) {
